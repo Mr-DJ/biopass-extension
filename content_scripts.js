c2054d7440c6edb22dc53434d6092ad4422f5102
Samuel Jonathan <jonathandinesh@gmail.com>
2023-02-04 20:08:13 +0530

    shrivardan popup

--- content_scripts.js
+++ content_scripts.js
@@ -6,7 +6,75 @@
   {
     const injectElement=document.createElement('div');
     injectElement.className='sauban';
-    injectElement.innerHTML='<html> <head> <style> html{ background-color: antiquewhite; border-radius: 20px; } button[type="submit"] { background-color: green; color: white; border-radius: 20px; padding: 10px 20px; border: none; cursor: pointer; margin-top: 10px; } button[type="reset"] { background-color: red; color: white; border-radius: 20px; padding: 10px 20px; border: none; cursor: pointer; margin-top: 10px; } input[type="text"], input[type="password"] { border-radius: 20px; border: 2px solid rgb(43, 103, 233); padding: 10px; } </style> </head><body><form><div><label for="username">Username:</label><input type="text" id="username" name="username"></div><div><label for="password">Password:</label><input type="password" id="password" name="password"></div><div><button type="submit">Save</button><button type="reset">Cancel</button></div></form>';
+    // injectElement.innerHTML =
+    //   '<html> <head> <style> html{ background-color: antiquewhite; border-radius: 20px; } button[type="submit"] { background-color: green; color: white; border-radius: 20px; padding: 10px 20px; border: none; cursor: pointer; margin-top: 10px; } button[type="reset"] { background-color: red; color: white; border-radius: 20px; padding: 10px 20px; border: none; cursor: pointer; margin-top: 10px; } input[type="text"], input[type="password"] { border-radius: 20px; border: 2px solid rgb(43, 103, 233); padding: 10px; } .shrivardan { display: none; } </style> </head><body><form class="shrivardan" ><div><label for="username">Username:</label><input type="text" id="sinwan_username" name="username"></div><div><label for="password">Password:</label><input type="password" id="sinwan_password" name="password"></div><div><button type="submit">Save</button><button type="reset">Cancel</button></div></form>';
+    
+    injectElement.innerHTML = `<html>
+ 
+      <head>
+        
+        <style>
+            html{
+              
+         background-color: antiquewhite;     
+        border-radius: 20px;
+      }
+            
+            button[type="submit"] {
+              background-color: green;
+              color: white;
+              border-radius: 20px;
+              padding: 10px 20px;
+              border: none;
+              cursor: pointer;
+              margin-top: 10px;
+            }
+
+            button[type="reset"] {
+            background-color: red;
+            color: white;
+            border-radius: 20px;
+            padding: 10px 20px;
+            border: none;
+            cursor: pointer;
+            margin-top: 10px;
+            }
+
+            input[type="text"], input[type="password"] {
+                border-radius: 20px;
+                border: 2px solid rgb(43, 103, 233);
+                padding: 10px;
+            }
+
+            .shrivardan {
+              display: none;
+            }
+            
+          </style>
+          
+      
+        </head>
+        
+        <body>
+        
+        <form class="shrivardan">
+            <div>
+              <label for="username">Username:</label>
+              <input type="text" id="username" name="username">
+            </div>
+            <div>
+              <label for="password">Password:</label>
+              <input type="password" id="password" name="password">
+            </div>
+            <div>
+              <button type="submit">Save</button>
+              <button type="reset">Cancel</button>
+            </div>
+          </form>
+          <script type="text/javascript" src="popup.js"></script>  
+    </body>
+    
+</html>`;
     document.body.appendChild(injectElement);
   }
   sex();
--- test.js
+++ test.js
@@ -1,45 +1,5 @@
-// document.body.style.border = "10px solid red";
-// console.log('Started');
 
-// let inputs = document.getElementsByTagName("input");
-// let len = inputs.length;
-
-// if (len == 0) {
-//   console.log("there is no form");
-// }
-// console.log("there might be a form");
-
-
-// function getLoginFields() {
-
-//     let pass = "", user = "";
-
-//     while (len--) {
-//         if (inputs[len].type === "password") {
-//             pass = inputs[len].value;
-//             user = (len > 0 && (inputs[len - 1].type === "text"  || inputs[len - 1].type === "email")) ? inputs[len - 1].value : user;
-//         }
-//     }
-
-//     if(user.length == 0 || pass.length == 0) {
-//         console.log('ure empty')
-//         // AHUYY??
-//     } else {
-//         console.log("password -> " + pass);
-//         console.log("user -> " + user);
-//     }
-// }
-
-// let submitButton = document.querySelectorAll("input[type=submit]")[0]
-//   ? document.querySelectorAll("input[type=submit]")[0]
-//   : document.querySelectorAll("button[type=submit]")[0];
-
-// // if (submitButton.click()) 
-// //     console.log(" you touched me owo");
-
-// submitButton.addEventListener('click', function(){ console.log("clicked"); getLoginFields(); });
-
-console.log("Started");
+console.log('Started');
 
 let inputs = document.getElementsByTagName("input");
 let len = inputs.length;
@@ -49,36 +9,77 @@ if (len == 0) {
 }
 console.log("there might be a form");
 
+
 function getLoginFields() {
-  let pass = "",
-    user = "";
 
-  while (len--) {
-    if (inputs[len].type === "password") {
-      pass = inputs[len].value;
-      user =
-        len > 0 &&
-        (inputs[len - 1].type === "text" || inputs[len - 1].type === "email")
-          ? inputs[len - 1].value
-          : user;
+    let pass = "", user = "";
+
+    while (len--) {
+        if (inputs[len].type === "password") {
+            pass = inputs[len].value;
+            user = (len > 0 && (inputs[len - 1].type === "text"  || inputs[len - 1].type === "email")) ? inputs[len - 1].value : user;
+        }
     }
-  }
 
-  if (user.length == 0 || pass.length == 0) {
-    console.log("ure empty");
-  } else {
-    console.log("password -> " + pass);
-    console.log("user -> " + user);
-    document.getElementById("username").innerHTML = user;
-    document.getElementById("password").innerHTML = pass;
-  }
+    if(user.length == 0 || pass.length == 0) {
+        console.log('ure empty')
+        // AHUYY??
+    } else {
+        console.log("password -> " + pass);
+        console.log("user -> " + user);
+    }
 }
 
-let submitButton =
-  document.querySelectorAll("input[type=submit]")[0] ||
-  document.querySelectorAll("button[type=submit]")[0];
+let submitButton = document.querySelectorAll("input[type=submit]")[0]
+  ? document.querySelectorAll("input[type=submit]")[0]
+  : document.querySelectorAll("button[type=submit]")[0];
 
-submitButton.addEventListener("click", function() {
-  console.log("clicked");
-  getLoginFields();
+
+submitButton.addEventListener('click', function() { 
+    console.log("clicked"); getLoginFields();
+    document.getElementsByClassName('shrivardan')[0].style.display = "block";
 });
+
+// console.log("Started");
+
+// let inputs = document.getElementsByTagName("input");
+// let len = inputs.length;
+
+// if (len == 0) {
+//   console.log("there is no form");
+// }
+// console.log("there might be a form");
+
+// function getLoginFields() {
+//   let pass = "",
+//     user = "";
+
+//   while (len--) {
+//     if (inputs[len].type === "password") {
+//       pass = inputs[len].value;
+//       user =
+//         len > 0 &&
+//         (inputs[len - 1].type === "text" || inputs[len - 1].type === "email")
+//           ? inputs[len - 1].value
+//           : user;
+//     }
+//   }
+
+//   if (user.length == 0 || pass.length == 0) {
+//     console.log("ure empty");
+//   } else {
+//     console.log("password -> " + pass);
+//     console.log("user -> " + user);
+//     document.getElementById("username").innerHTML = user;
+//     document.getElementById("password").innerHTML = pass;
+//   }
+// }
+
+// let submitButton =
+//   document.querySelectorAll("input[type=submit]")[0] ||
+//   document.querySelectorAll("button[type=submit]")[0];
+
+// submitButton.addEventListener("click", function() {
+//   console.log("clicked");
+//   getLoginFields();
+// });
