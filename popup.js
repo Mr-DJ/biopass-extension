c56b0e9f4d1c6716d62be103923e1a9575be7215
Samuel Jonathan <jonathandinesh@gmail.com>
2023-03-02 17:53:02 +0530

    manifest

--- popup.js
+++ popup.js
@@ -3,6 +3,17 @@ document.querySelector('form').addEventListener('submit', function (event) {
     let inputValue = document.querySelector('#input').value;
     
     alert('You entered: ' + inputValue);
-
+    getWebsiteName()
     //WRITE SEND BACKEND CODE HERE
 });
+
+
+const getWebsiteName = function() {
+  console.log(document.URL.replace(/.+\/\/|www.|\..+/g, ""));
+  // console.log(document.URL.replace(/.+\/\/|www.|\..+/g, ""));
+}
+
+const getHostname = (url) => {
+  // use URL constructor and return hostname
+  return new URL(url).hostname;
+};
