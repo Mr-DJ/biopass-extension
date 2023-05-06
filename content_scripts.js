bb62dcc54231d2a0c29442cb9f9f6dc1ee23cf2a
Samuel Jonathan <jonathandinesh@gmail.com>
2023-05-06 15:47:14 +0530

    styles

--- content_scripts.js
+++ content_scripts.js
@@ -6,59 +6,67 @@ const initiateExt = function() {
   injectElement.innerHTML = `<html>
     <head>
       <style>
-          html {  
-            border-radius: 20px;
+          .sauban {
+            padding: 7px;
+            width: fit-content;
+            position: fixed;
+            top: 10px;
+            right: 10px;
+            font-family: "Lucida Console", "Courier New", monospace;
+            background-color: lightgrey;
+          }
+          
+          .shrivardan input {
+            border-radius: 0;
+            padding: 5px;
+          }
+          
+          .shrivardan div {
+            display: flex;
+            justify-content: space-between;
+            padding-bottom: 2px;
+          }
+          
+          .shrivardan button {
+            color: white;
+            padding: 10px 20px;
+            border: none;
+            margin-top: 10px;
+            cursor: pointer;
           }
 
-          button[type="submit"] {
+          .shrivardan button[type="submit"] {
             background-color: green;
-            color: white;
-            border-radius: 20px;
-            padding: 10px 20px;
-            border: none;
-            cursor: pointer;
-            margin-top: 10px;
           }
-
-          button[type="reset"] {
+    
+          .shrivardan button[type="reset"] {
             background-color: red;
-            color: white;
-            border-radius: 20px;
-            padding: 10px 20px;
-            border: none;
-            cursor: pointer;
-            margin-top: 10px;
           }
 
-          input[type="text"], input[type="password"] {
-            border-radius: 20px;
-            border: 2px solid rgb(43, 103, 233);
-            padding: 10px;
+          .shrivardan .inline {
+            display: inline;
           }
 
-          .shrivardan {
-            display: none;
-          }
-        </style>    
-      </head>
-
-      <body>
-        <form class="shrivardan">
-          <div>
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" class="shriv_inputs">
-          </div>
-          <div>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" class="shriv_inputs">
-          </div>
-          <div>
-            <button id="subb" type="submit"">Save</button>
-            <button id="canc" type="reset">Cancel</button>
-          </div>
-        </form>
+      </style>    
+    </head>
+          
+    <body>
+      <form class="shrivardan">
+        <div>
+          <label for="username">Username:</label>
+          <input type="text" id="username" name="username" class="shriv_inputs">
+        </div>
+        <div>
+          <label for="password">Password:</label>
+          <input type="password" id="password" name="password" class="shriv_inputs">
+        </div>
+        <div class="inline">
+          <button id="subb" type="submit"">Save</button>
+          <button id="canc" type="reset">Cancel</button>
+        </div>
+      </form>
         <script type="text/javascript" src="popup.js"></script>
-      </body>
+    </body>
 </html>`;
   document.body.appendChild(injectElement);
 }
